68c9fefb4ac7913d94f221a8bb55d50dfefae473
Lungelo Nkosi <LNkosi@hollywoodbets.net>
2023-01-19 14:38:38 +0200

    Add em Up Logic

--- winner/winner.cs
+++ winner/winner.cs
@@ -8,166 +8,138 @@ namespace winner
 {
     public class winner
     {
-        private static readonly string projectPath = System.IO.Directory.GetCurrentDirectory();
-        //projectPath
-        private static  string currentDirectoryPath = projectPath.Remove(projectPath.IndexOf("winner"))+ "winner"+"\\winner";
-        //var actualPath = projectPath + $@"HISDatabase\\{schema}\Stored Procedures";
-        
-        static void Main(string[] args)
+        private static void Main(string[] args)
+        {
+            string inputFile = GetParamValue(args, "--in");
+            string outFile = GetParamValue(args, "--out");
+
+            try
+            {
+                if (!string.IsNullOrEmpty(inputFile) && !string.IsNullOrEmpty(outFile))
+                {
+                    IEnumerable<Player> players = GetPlayers(inputFile, outFile);
+
+                    OutputResult(GetWinnerName(players), outFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                OutputResult($"Exception : {ex.Message}", outFile);
+            }
+            Console.ReadKey();
+        }
+
+        private static string GetParamValue(string[] parameters, string name)
         {
             try
             {
-                //string inputfile ="abc.txt";
-                string inputfile = (string)args[1].ToString();
-                //string outfile = "xyz.txt";
-                string outfile = (string)args[3].ToString();
-
-                Console.WriteLine(inputfile);
-                Console.WriteLine(outfile);
-
-                string filenamer = Path.Combine(currentDirectoryPath, inputfile);
-                var playersInput = new Dictionary<string, int> { };
-                using (var sr = new StreamReader(filenamer))
+                if (parameters.Any(p => p.Equals(name)))
                 {
-                    string line, playerName;
-                    List<string> scoreResults = new List<string>();
-                    List<string> nameResults = new List<string>();
+                    int indexOfParamName = Array.IndexOf(parameters, name);
+                    if (indexOfParamName == parameters.Length - 1)
+                    {
+                        Console.WriteLine($"Exception : Start-up param {name} has no value, please provide with value e.g. {name} pathToFile.txt");
+                        return null;
+                    }
+                    else
+                    {
+                        return parameters[indexOfParamName + 1];
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Exception : Cannot find parameter {name}");
+                }
+            }
+
+            catch (Exception ex) { Console.WriteLine($"Exception : {ex.Message}"); }
+            return null;
+        }
+
+        private static List<Player> GetPlayers(string inputFilePath, string outputFilePath)
+        {
+            List<Player> players = new List<Player>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(inputFilePath))
+                {
+                    string line;
                     while ((line = sr.ReadLine()) != null)
                     {
-                        if (!String.IsNullOrWhiteSpace(line))
+                        if (!string.IsNullOrWhiteSpace(line))
                         {
-                            int colonLocation = line.IndexOf(':', StringComparison.Ordinal);
-                            if (colonLocation > 0)
+                            if (HasPlayerName(line))
                             {
-                                playerName = line.Substring(0, colonLocation);
+                                Player player = new Player(GetPlayerName(line));
 
-                                string Card1 = GetCard(colonLocation, 1, line);
-                                List<char> Card1List = new List<char>();
-                                Card1List.AddRange(Card1);
-
-                                string Card2 = GetCard(colonLocation, 4, line);
-                                List<char> Card2List = new List<char>();
-                                Card2List.AddRange(Card2);
-
-                                string Card3 = GetCard(colonLocation, 7, line);
-                                List<char> Card3List = new List<char>();
-                                Card3List.AddRange(Card3);
-
-                                string Card4 = GetCard(colonLocation, 10, line);
-                                List<char> Card4List = new List<char>();
-                                Card4List.AddRange(Card4);
-
-                                string Card5 = GetCard(colonLocation, 13, line);
-                                List<char> Card5List = new List<char>();
-                                Card5List.AddRange(Card5);
-
-                                int cardNumberTotal = GetCardValue(Card1List[0].ToString()) + GetCardValue(Card2List[0].ToString()) +
-                                    GetCardValue(Card3List[0].ToString()) + GetCardValue(Card4List[0].ToString()) + GetCardValue(Card5List[0].ToString());
-
-                                 var CalcSuitTotal = GetSuitValue(Card1List[1].ToString()) + GetSuitValue(Card2List[1].ToString()) + GetSuitValue(Card3List[1].ToString())
-                                    + GetSuitValue(Card4List[1].ToString()) + GetSuitValue(Card5List[1].ToString());
-
-                                playersInput.Add(playerName + "#" + CalcSuitTotal, cardNumberTotal);
-
-                                var cardTotalValueWinners = GetTotalWinners(playersInput);
-
-                                if (cardTotalValueWinners.Count > 1)
+                                foreach (string card in GetPlayerCards(line))
                                 {
-                                    var suitTotals = SplitSuitPlayerResult(cardTotalValueWinners);
-                                    cardTotalValueWinners = GetTotalWinners(suitTotals);
+                                    player.CardScores.Add(GetCardValue(card));
+                                    player.SuitScores.Add(GetSuitValue(card));
                                 }
 
-                                string _gameResults = ProcessResults(cardTotalValueWinners);
+                                players.Add(player);
 
-                                OutPutResult(_gameResults, outfile);
+                                //Console.WriteLine($"Player: {player.Name}, Card Total: {player.CardScoreTotal}, Suit Score: {player.SuitScoreTotal}, Cards :: {string.Join(", ", GetPlayerCards(line))}");
                             }
                             else
                             {
-                                OutPutResult("ERROR", outfile);
+                                OutputResult("ERROR", outputFilePath);
                             }
                         }
                     }
                 }
-
-
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) { OutputResult($"An error occurred, Exception: {ex.Message}", outputFilePath); }
 
-                 throw ex;
-            }
-         
+            return players;
         }
 
-        public static string ProcessResults(Dictionary<string, int> playersKeyValuePair)
+        private static string GetWinnerName(IEnumerable<Player> players)
         {
-            var results = new StringBuilder();
-            int tiescore = playersKeyValuePair.Values.Max();
-            if (playersKeyValuePair.Count > 1)
-            {
-                foreach (KeyValuePair<string, int> player in playersKeyValuePair)
-                {
-                    if (results.Length > 0)
-                    {
-                        results.Append($", {player.Key}");
-                    }
-                    else
-                    {
-                        results.Append($"{player.Key}");
-                    }
+            string winnerName = "";
+            //Sort by total card score then by suit score
+            players = players.OrderByDescending(p => p.CardScoreTotal).ThenByDescending(p => p.SuitScoreTotal);
 
+            //since players are ordered, the first in the list has to be the highest (by cards and suit),
+            // in case of a tie, any other winner has to have the same card score and suit score 
+            int suitScoreTotal = players.FirstOrDefault().SuitScoreTotal;
+
+            int countWinners = 0;
+            foreach (Player player in players)
+            {
+                if (player.CardScoreTotal == players.FirstOrDefault().CardScoreTotal
+                    && player.SuitScoreTotal == suitScoreTotal)
+                {
+                    winnerName += (countWinners > 0) ? $",{player.Name}" : player.Name;
+                    countWinners++;
                 }
             }
-            else
-            {
-                var winnerName = playersKeyValuePair.ElementAt(0).Key.ToString();
-                results.Append($"{winnerName.Substring(0, winnerName.IndexOf("#"))}");
-            }
-            results.Append($": {tiescore}");
-            return results.ToString();
+
+            return $"{winnerName}:{players.FirstOrDefault().CardScoreTotal}";
         }
-        public static Dictionary<string, int> GetTotalWinners(Dictionary<string, int> playersKeyValuePair)
+
+        private static string GetPlayerName(string line)
         {
-            var results = new Dictionary<string, int>();
-            int highestCardTotal = playersKeyValuePair.Values.Max();
-
-            foreach (KeyValuePair<string, int> player in playersKeyValuePair)
-            {
-                if (player.Value == highestCardTotal)
-                {
-                    results.Add(player.Key, player.Value);
-                }
-            }
-            return results;
+            return line.Substring(0, line.IndexOf(':', StringComparison.Ordinal));
         }
 
-       public static Dictionary<string, int> SplitSuitPlayerResult(Dictionary<string, int> keyValuePairs)
+        private static bool HasPlayerName(string line)
         {
-            Dictionary<string, int> result = new Dictionary<string, int>();
-            foreach (KeyValuePair<string, int> kvp in keyValuePairs)
-            {
-                result.Add(kvp.Key.Split("#")[0], Int32.Parse(kvp.Key.Split("#")[1]));
-            }
-            return result;
+            return line.IndexOf(':', StringComparison.Ordinal) > 0;
         }
 
-        public static void OutPutResult(string gameResuslt,string outputFile)
+        public static void OutputResult(string message, string outputFile)
         {
             try
             {
-                Console.WriteLine(gameResuslt);
-                string filename = Path.Combine(currentDirectoryPath, outputFile);
-                File.Delete(filename);
-                StreamWriter sw = new StreamWriter(filename, true, Encoding.ASCII);
+                Console.WriteLine(message);
+                if (File.Exists(outputFile)) { File.Delete(outputFile); }
 
-                if ("ERROR".Equals(gameResuslt))
-                {
-                    sw.Write(gameResuslt);
-                }
-                else
-                {
-                    sw.Write(gameResuslt);
-                }
+                StreamWriter sw = new StreamWriter(outputFile, true, Encoding.ASCII);
+
+                sw.Write(message);
+
                 sw.Close();
             }
             catch (Exception e)
@@ -177,9 +149,41 @@ namespace winner
             finally { }
         }
 
-        public static int GetSuitValue(string CardGroup)
+        private static string[] GetPlayerCards(string line)
         {
-            var suitValue = CardGroup.ToUpper() switch
+            line = line.Substring(line.IndexOf(':', StringComparison.Ordinal) + 1);
+            return line.Split(',');
+        }
+
+        private static bool IsNumber(string cardFace)
+        {
+            return int.TryParse(cardFace, out int n);
+        }
+
+        public static int GetCardValue(string card)
+        {
+            string cardNumber = card.Substring(0, card.Length - 1);
+
+            if (IsNumber(cardNumber))
+            {
+                return int.Parse(cardNumber);
+            }
+            else
+            {
+                switch (cardNumber.ToUpper())
+                {
+                    case "A":
+                    case "J": return 11;
+                    case "Q": return 12;
+                    case "K": return 13;
+                    default: return 0;
+                }
+            }
+        }
+
+        public static int GetSuitValue(string card)
+        {
+            return card.Substring(card.Length - 1).ToUpper() switch
             {
                 "H" => 3,
                 "S" => 4,
@@ -187,42 +191,24 @@ namespace winner
                 "C" => 1,
                 _ => 0,
             };
-            return suitValue;
-        }
-        static string GetCard(int colonLocation, int cardPposition, string lineItem)
-        {
-            return lineItem.Substring(colonLocation + cardPposition, 2);
-        }
-        public static int GetCardValue(string CardNumber)
-        {
-            int cardValue = 0;
-            if (!(CardNumber.ToString().Equals("A")) && !(CardNumber.ToString().Equals("J")) && !(CardNumber.ToString().Equals("K")) && !(CardNumber.ToString().Equals("Q")))
-            {
-                cardValue = int.Parse(CardNumber.ToString());
-            }
-            else
-            {
-                switch (CardNumber)
-                {
-                    case "A":
-                        cardValue = 1;
-                        break;
-                    case "J":
-                        cardValue = 11;
-                        break;
-                    case "Q":
-                        cardValue = 12;
-                        break;
-                    case "K":
-                        cardValue = 13;
-                        break;
-                }
-            }
-
-
-            return cardValue;
         }
     }
 
-   
+    public record Player
+    {
+        public string Name { get; set; }
+
+        public List<int> CardScores = new List<int>();
+
+        public List<int> SuitScores = new List<int>();
+
+        public int CardScoreTotal => CardScores.Sum();
+
+        public int SuitScoreTotal => SuitScores.Sum();
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
 }
